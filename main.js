97e3256af2ff8ce446b44b71ca30309ce1a4581a
Спащанский Игорь Александрович <reunion@kontur>
2018-05-26 00:58:08 +0500

    pos fix

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
       Axis.sum = sum;
       Axis.imgSrc = 'arrow.svg';
       Axis.createEquation(Axis.firstTerm, Axis.secondTerm);
-      Axis.createInterval(a, 'firstTerm');
+      Axis.createInterval(a, ['interval', 'first-interval']);
 
       Axis.bindEvents();
 
@@ -22,52 +22,64 @@ $(document).ready(function () {
     },
 
     bindEvents: function() {
-      $('#' + Axis.target +'_firstTerm').keyup(Axis.inputHandler);
-      $('#' + Axis.target +'_secondTerm').keyup(Axis.inputHandler);
-      $('#' + Axis.target + '_sum').keyup(Axis.equationHandler);
+      $('.' + Axis.target +'_interval' + ' input').keyup(Axis.inputHandler);
+      $('.' + Axis.target + '_sum').keyup(Axis.equationHandler);
     },
     
     createEquation: function(firstTerm, secondTerm) {
-      var a = createEquationElement(firstTerm, 'term', 'first');
-      var b = createEquationElement(secondTerm, 'term', 'second');
+      var a = createEquationElement(firstTerm, ['term', 'first']);
+      var b = createEquationElement(secondTerm, ['term', 'second']);
 
       $('#' + Axis.target + ' .equation').append(a, [
-        createEquationElement('+', 'action', 'plus'),
+        createEquationElement('+', ['action', 'plus']),
         b,
-        createEquationElement('=', 'action', 'equal'),
-        createEquationElement('?', 'result', 'sum')
+        createEquationElement('=', ['action', 'equal']),
+        createEquationElement('?', ['result', 'sum'])
       ]);
     },
 
-    createInterval: function (length, id) {
+    createInterval: function (length, classNames) {
       var interval = document.createElement('div');
       var input = document.createElement('input');
       var img = document.createElement('img');
-      $(interval)
-        .attr('id', id)
-        .attr('class', 'interval');
+
+      $(interval).addClass('interval');
+
+      classNames.forEach(function(className) {
+        $(interval).addClass(Axis.target + '_' + className);
+      });
+
       $(input)
-        .attr('id', Axis.target + '_' + id)
+        //.attr('class', Axis.target + '_' + id)
         .attr('type', 'text');
+
       $(img)
         .attr('src', Axis.imgSrc)
         .attr('width', length*Axis.intervalSizeX)
         .attr('height', length*Axis.intervalSizeY);
+
       $(interval).append(input, img);
+
       $('#' + Axis.target + ' .intervals-box').append(interval);
     },
     
     inputHandler: function (data) {
-      //console.log(data);
-      var id = data.target.attributes['id'].nodeValue;
-      var key = id.split('_')[1];
-      if (+data.key === Axis[key]) {
-        $('#' + Axis.target + '_' + key.slice(0, -4)).css('background-color', 'white');
-        $(data.target).replaceWith('<p>' + Axis[key] + '</p>');
-        Axis.nextStep(id);
+      var target = data.target;
+
+      if(data.key == +data.key) {
+        $(target).val(data.key);
       } else {
-        console.log(key.slice(0, -4));
-        $('#' + Axis.target + '_' + key.slice(0, -4)).css('background-color', 'orange');
+        $(target).val('');
+      }
+
+      var term = $(target).parent().hasClass(Axis.target + '_first-interval') ? 'firstTerm' : 'secondTerm';
+
+      if (+data.key === Axis[term]) {
+        $('.' + Axis.target + '_' + term.slice(0, -4)).css('background-color', 'white');
+        $(data.target).replaceWith('<p>' + Axis[term] + '</p>');
+        Axis.nextStep(term);
+      } else {
+        $('.' + Axis.target + '_' + term.slice(0, -4)).css('background-color', 'orange');
         $(data.target).css('color', 'red');
       }
     },
@@ -80,13 +92,13 @@ $(document).ready(function () {
       }
     },
 
-    nextStep: function (id) {
-      console.log(id);
-      if(id.slice(-9) === 'firstTerm') {
-        Axis.createInterval(Axis.secondTerm, 'secondTerm');
+    nextStep: function (term) {
+      if(term === 'firstTerm') {
+        Axis.createInterval(Axis.secondTerm, ['interval', 'second-interval']);
+        $('.' + Axis.target + '_second-interval').css('left', Axis.intervalSizeX*Axis.firstTerm);
         Axis.bindEvents();
       } else {
-        $('#' + Axis.target + '_sum').replaceWith('<input id="' + Axis.target + '_sum"' + ' ' + 'class=' + '"equation__sum' + '">');
+        $('.' + Axis.target + '_sum').replaceWith('<input class="' + Axis.target + '_sum equation__sum">');
         Axis.bindEvents();
       }
     }
@@ -99,12 +111,13 @@ $(document).ready(function () {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
-  function createEquationElement(value, className, id) {
+  function createEquationElement(value, classNames) {
     var elem = document.createElement('div');
     elem.appendChild(document.createTextNode(value));
-    $(elem)
-      .attr('id', Axis.target + '_' + id)
-      .attr('class', 'equation__'+className);
+    $(elem).addClass('equation__elem')
+    classNames.forEach(function(className) {
+      $(elem).addClass(Axis.target + '_' + className);
+    });
     return elem;
   }
 
